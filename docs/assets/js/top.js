19186ab6242cf7592e707be3649b3192299549f4
Jason Chafin <jchafin@ucsc.edu>
2023-04-28 11:40:48 -0700

    Lots of hacking

--- docs/assets/js/top.js
+++ docs/assets/js/top.js
@@ -1,24 +1,27 @@
 
-let offset = 100;
-let speed = 250;
-let duration = 500;
-let toTop = document.querySelector(".topbutton");
+
 window.addEventListener('load', function (){
-	alert("it's loaded!")
+	alert("it's loaded!");
+	let offset = 100;
+	let speed = 250;
+	let duration = 500;
+	let toTop = document.querySelector(".topbutton");
+	let btnVisibility = () => {
+		if (window.scrollY < offset) {
+			toTop.style.visibility = "hidden";
+		} else {
+			toTop.style.visibility = "visible";
+		}
+	};
+	document.addEventListener("scroll", () => {
+		btnVisibility();
+	});
+	toTop.addEventListener('click', function (e) {
+		e.preventDefault();
+		window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+	});
 });
-let btnVisibility = () => {
-	if (window.scrollY < offset) {
-		toTop.style.visibility = "hidden";
-	} else {
-	 	toTop.style.visibility = "visible";
-	 }
-};
-document.addEventListener("scroll", () => {
-	btnVisibility();
-});
-toTop.addEventListener('click', function (e) {
-	e.preventDefault();
-	window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-});
+
+
 
 console.log("Hello World");
