22f11202a39608a54610ffe7bc57b65b4f2883ec
Jason Chafin <jchafin@ucsc.edu>
2023-04-28 11:32:18 -0700

    Loaded!

--- docs/assets/js/top.js
+++ docs/assets/js/top.js
@@ -3,12 +3,15 @@ let offset = 100;
 let speed = 250;
 let duration = 500;
 let toTop = document.querySelector(".topbutton");
+window.addEventListener('load', function (){
+	alert("it's loaded!")
+});
 let btnVisibility = () => {
 	if (window.scrollY < offset) {
 		toTop.style.visibility = "hidden";
-	}// else {
-	// 	toTop.style.visibility = "hidden";
-	// }
+	} else {
+	 	toTop.style.visibility = "visible";
+	 }
 };
 document.addEventListener("scroll", () => {
 	btnVisibility();
