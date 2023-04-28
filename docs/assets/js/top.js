eda20ffb068e0df498e08e88efbed8ceee6f6d84
Jason Chafin <jchafin@ucsc.edu>
2023-04-28 10:37:06 -0700

    to top js css edits

--- docs/assets/js/top.js
+++ docs/assets/js/top.js
@@ -3,7 +3,16 @@ let offset = 100;
 let speed = 250;
 let duration = 500;
 let toTop = document.querySelector("a.topbutton");
-
+let btnVisibility = () => {
+	if (window.scrollY > 400) {
+		toTop.style.visibility = "visible";
+	} else {
+		toTop.style.visibility = "hidden";
+	}
+};
+document.addEventListener("scroll", () => {
+	btnVisibility();
+});
 toTop.addEventListener('click', function (e) {
 	e.preventDefault();
 	window.scroll({ top: 0, left: 0, behavior: 'smooth' });
